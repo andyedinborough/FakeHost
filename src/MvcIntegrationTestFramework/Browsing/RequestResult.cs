a0674654b959f59606f6a5b944d0a04ce2ea1611
Andy Edinborough <andy@edinborough.org>
2012-05-09 00:47:04 -0500

    refactoring

--- src/MvcIntegrationTestFramework/Browsing/RequestResult.cs
+++ src/MvcIntegrationTestFramework/Browsing/RequestResult.cs
@@ -1,16 +1,14 @@
 using System.Web;
 using System.Web.Mvc;
 
-namespace MvcIntegrationTestFramework.Browsing
-{
-    /// <summary>
-    /// Represents the result of a simulated request
-    /// </summary>
-    internal class RequestResult
-    {
-        public HttpResponse Response { get; set; }
-        public string ResponseText { get; set; }
-        public ActionExecutedContext ActionExecutedContext { get; set; }
-        public ResultExecutedContext ResultExecutedContext { get; set; }        
-    }
+namespace MvcIntegrationTestFramework.Browsing {
+  /// <summary>
+  /// Represents the result of a simulated request
+  /// </summary>
+  internal class RequestResult {
+    public HttpResponse Response { get; set; }
+    public string ResponseText { get; set; }
+    public ActionExecutedContext ActionExecutedContext { get; set; }
+    public ResultExecutedContext ResultExecutedContext { get; set; }
+  }
 }
